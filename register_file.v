3712592614ba03a2b4fe2beb2381f98406ee4990
lewisf <lewis.f.chung@gmail.com>
2013-02-03 20:58:23 -0800

    added timescale and changed formatting'

--- register_file.v
+++ register_file.v
@@ -1,12 +1,15 @@
+`timescale 1ns / 1ps
 // This is a Verilog description for a register file
 
 module register_file
-  (input clk,							// clock
+(
+	input clk,							// clock
    input copyout,						// are we copying value from a res into a register
 	input [2:0] reg_sel,			   // select a register
 	output [15:0] res_val,		   // value of res
 	output [15:0] reg_val,			// value of specified register
-	input [15:0] write_data);     // write data from alu
+	input [15:0] write_data			// write data from alu
+);
   
    reg [15:0] reg0, reg1, reg2, reg3, reg4, reg5, reg6, reg7, reg8, res;
 	
@@ -25,5 +28,4 @@ module register_file
 		res <= write_data;
 	end
 
-  
 endmodule
