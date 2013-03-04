794814c7662deb8b85ba7c02c59515e2e4fd59af
Derek Louie <dlouie91@gmail.com>
2013-03-03 17:36:33 -0800

    Changed control, dataram, processor, added a cycle counter

--- processor.v
+++ processor.v
@@ -14,7 +14,7 @@
 
 // PROGRAM		"Quartus II"
 // VERSION		"Version 11.0 Build 208 07/03/2011 Service Pack 1 SJ Web Edition"
-// CREATED		"Sun Mar 03 14:53:00 2013"
+// CREATED		"Sun Mar 03 17:32:22 2013"
 
 module processor(
 	restart,
@@ -39,21 +39,21 @@ wire	sign;
 wire	SYNTHESIZED_WIRE_0;
 wire	SYNTHESIZED_WIRE_1;
 wire	SYNTHESIZED_WIRE_2;
-wire	SYNTHESIZED_WIRE_3;
+wire	SYNTHESIZED_WIRE_27;
 wire	SYNTHESIZED_WIRE_4;
-wire	[15:0] SYNTHESIZED_WIRE_26;
-wire	[15:0] SYNTHESIZED_WIRE_27;
-wire	[15:0] SYNTHESIZED_WIRE_9;
 wire	[15:0] SYNTHESIZED_WIRE_28;
+wire	[15:0] SYNTHESIZED_WIRE_29;
+wire	[15:0] SYNTHESIZED_WIRE_9;
+wire	[15:0] SYNTHESIZED_WIRE_30;
 wire	[15:0] SYNTHESIZED_WIRE_12;
 wire	[15:0] SYNTHESIZED_WIRE_13;
 wire	[15:0] SYNTHESIZED_WIRE_15;
 wire	[1:0] SYNTHESIZED_WIRE_16;
 wire	SYNTHESIZED_WIRE_17;
 wire	SYNTHESIZED_WIRE_18;
-wire	SYNTHESIZED_WIRE_29;
-wire	[15:0] SYNTHESIZED_WIRE_20;
-wire	SYNTHESIZED_WIRE_24;
+wire	SYNTHESIZED_WIRE_31;
+wire	[15:0] SYNTHESIZED_WIRE_21;
+wire	SYNTHESIZED_WIRE_25;
 
 
 
@@ -62,18 +62,22 @@ assign	SYNTHESIZED_WIRE_2 = SYNTHESIZED_WIRE_0 | SYNTHESIZED_WIRE_1;
 
 
 control	b2v_control(
-	.clk(clock),
 	.format(format),
 	.sign(sign),
 	.opcode(opcode),
 	.cpin(SYNTHESIZED_WIRE_17),
 	.cpout(SYNTHESIZED_WIRE_18),
-	.memRead(SYNTHESIZED_WIRE_3),
+	.memRead(SYNTHESIZED_WIRE_27),
 	.memWrite(SYNTHESIZED_WIRE_4),
 	.halt(done),
-	.branch(SYNTHESIZED_WIRE_29),
+	.branch(SYNTHESIZED_WIRE_31),
 	.jump(SYNTHESIZED_WIRE_0),
 	.writeSrc(SYNTHESIZED_WIRE_16));
+
+
+cycle_counter	b2v_inst(
+	.start(init),
+	.clk(clock));
 
 
 mux2	b2v_inst1(
@@ -85,10 +89,10 @@ mux2	b2v_inst1(
 
 data_ram	b2v_inst12(
 	.clk(clock),
-	.ReadMem(SYNTHESIZED_WIRE_3),
+	.ReadMem(SYNTHESIZED_WIRE_27),
 	.WriteMem(SYNTHESIZED_WIRE_4),
-	.DataAddress(SYNTHESIZED_WIRE_26),
-	.DataIn(SYNTHESIZED_WIRE_27),
+	.DataAddress(SYNTHESIZED_WIRE_28),
+	.DataIn(SYNTHESIZED_WIRE_29),
 	.DataOut(SYNTHESIZED_WIRE_12));
 
 
@@ -97,9 +101,9 @@ ALU	b2v_inst2(
 	.eq(sign),
 	.ltgt(instr_out),
 	.op(opcode),
-	.register(SYNTHESIZED_WIRE_27),
-	.res(SYNTHESIZED_WIRE_26),
-	.compres(SYNTHESIZED_WIRE_24),
+	.register(SYNTHESIZED_WIRE_29),
+	.res(SYNTHESIZED_WIRE_28),
+	.compres(SYNTHESIZED_WIRE_25),
 	.out(SYNTHESIZED_WIRE_15));
 
 
@@ -108,16 +112,16 @@ next_pc_logic	b2v_inst3(
 	.restart(restart),
 	.clock(clock),
 	.pc_in(SYNTHESIZED_WIRE_9),
-	.out(SYNTHESIZED_WIRE_28));
+	.out(SYNTHESIZED_WIRE_30));
 
 
 adder	b2v_inst4(
-	.a(SYNTHESIZED_WIRE_28),
+	.a(SYNTHESIZED_WIRE_30),
 	.out(pcplusone));
 
 
 instr_rom_1	b2v_instr_rom(
-	.pc_in(SYNTHESIZED_WIRE_28),
+	.pc_in(SYNTHESIZED_WIRE_30),
 	.format(format),
 	.sign(sign),
 	.immediate(immediate),
@@ -128,36 +132,36 @@ instr_rom_1	b2v_instr_rom(
 mux4	b2v_mux4(
 	.in0(SYNTHESIZED_WIRE_12),
 	.in1(SYNTHESIZED_WIRE_13),
-	.in2(SYNTHESIZED_WIRE_26),
+	.in2(SYNTHESIZED_WIRE_28),
 	.in3(SYNTHESIZED_WIRE_15),
 	.sel(SYNTHESIZED_WIRE_16),
-	.out(SYNTHESIZED_WIRE_20));
+	.out(SYNTHESIZED_WIRE_21));
 
 
 register_file	b2v_register_file(
 	.clk(clock),
 	.cpyin(SYNTHESIZED_WIRE_17),
 	.cpyout(SYNTHESIZED_WIRE_18),
-	.comp(SYNTHESIZED_WIRE_29),
+	.comp(SYNTHESIZED_WIRE_31),
+	.memLoad(SYNTHESIZED_WIRE_27),
 	.reg_sel(instr_out),
-	.write_data(SYNTHESIZED_WIRE_20),
-	.reg_val(SYNTHESIZED_WIRE_27),
-	.res_val(SYNTHESIZED_WIRE_26));
+	.write_data(SYNTHESIZED_WIRE_21),
+	.reg_val(SYNTHESIZED_WIRE_29),
+	.res_val(SYNTHESIZED_WIRE_28));
 
 
 mux2	b2v_res_or_reg(
-	.sel(SYNTHESIZED_WIRE_29),
-	.in0(SYNTHESIZED_WIRE_27),
-	.in1(SYNTHESIZED_WIRE_26),
+	.sel(SYNTHESIZED_WIRE_31),
+	.in0(SYNTHESIZED_WIRE_29),
+	.in1(SYNTHESIZED_WIRE_28),
 	.out(res));
 
 
 sign_extender	b2v_sign_extender(
-	.clk(clock),
 	.extend(immediate),
 	.extended(SYNTHESIZED_WIRE_13));
 
-assign	SYNTHESIZED_WIRE_1 = SYNTHESIZED_WIRE_24 & SYNTHESIZED_WIRE_29;
+assign	SYNTHESIZED_WIRE_1 = SYNTHESIZED_WIRE_25 & SYNTHESIZED_WIRE_31;
 
 
 endmodule
